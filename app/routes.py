6fcd127c1f87fd5fe128ce8e97ec316bc567a9ef
Rainacam <rainacam12@gmail.com>
2023-04-20 11:16:11 -0400

    defined planet class and instances

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,16 @@
 from flask import Blueprint
 
+class Planet:
+    def __init__(self, id="",name="", description=None, size=0):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.size = size
+
+mercury = Planet()
+saturn = Planet()
+pluto = Planet()
+ceres = Planet()
+makemake = Planet()
+haumea = Planet()
+eris = Planet()
