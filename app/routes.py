cec713a53dcd1c3005e5a74069a40c08946513ce
Rainacam <rainacam12@gmail.com>
2023-04-28 11:26:33 -0400

    configured db

--- app/routes.py
+++ app/routes.py
@@ -1,32 +1,32 @@
 from flask import Blueprint, jsonify
 
-class Planet:
-    def __init__(self, id="",name="", description=None, size=0):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.size = size
+# class Planet:
+#     def __init__(self, id="",name="", description=None, size=0):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.size = size
 
 
-planets = [
-    Planet(1, "Pluto", "Heart-shaped glacier", "Dwarf"),
-    Planet(2, "Ceres", "Named for Roman Goddess of corn and harvests", "Dwarf"),
-    Planet(3, "Makemake", "Bright and donut-shaped", "Dwarf")
-]
+# planets = [
+#     Planet(1, "Pluto", "Heart-shaped glacier", "Dwarf"),
+#     Planet(2, "Ceres", "Named for Roman Goddess of corn and harvests", "Dwarf"),
+#     Planet(3, "Makemake", "Bright and donut-shaped", "Dwarf")
+# ]
 
 planet_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
 # create endpoint to get resources
-@planet_bp.route("", methods=["GET"])
+# @planet_bp.route("", methods=["GET"])
 
-def handle_planets():
-    planet_response = []
-    for planet in planets:
-        planet_response.append({
-            "id": planet.id,
-            "name": planet.name,
-            "description": planet.description,
-            "size": planet.size
-        })
+# def handle_planets():
+#     planet_response = []
+#     for planet in planets:
+#         planet_response.append({
+#             "id": planet.id,
+#             "name": planet.name,
+#             "description": planet.description,
+#             "size": planet.size
+#         })
 
-    return jsonify(planet_response)
+#     return jsonify(planet_response)
