cf3d2d18149bd903374a2b4b5a02a2a6f8ee2a7b
Rainacam <rainacam12@gmail.com>
2023-04-28 11:58:06 -0400

    created def handle_planets to post a new planet

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, jsonify, abort, make_response
+from flask import Blueprint, jsonify, abort, make_response, request
+from .models.planet import Planet
+from app import db
+
 
 # class Planet:
 #     def __init__(self, id="",name="", description=None, size=0):
@@ -30,3 +33,36 @@ planet_bp = Blueprint("planets", __name__, url_prefix="/planets")
 #         })
 
 #     return jsonify(planet_response)
+
+# define route for creating a planet
+@planet_bp.route("", methods=['POST'])
+def handle_planets():
+    request_body = request.get_json()
+
+    new_planet = Planet(
+        name = request_body["name"],
+        description = request_body["description"],
+        size = request_body["size"]
+    )
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} successfully created", 201)
+
+# define a route for getting all planet 
+@planet_bp.route("", methods=['GET'])
+def read_all_planets():
+    planets_response = []
+    # query.all gets planet from db
+    planets = Planet.query.all()
+
+    # add each planet to the response body
+    for planet in planets:
+        planets_response.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "size": planet.size
+        })
+    return jsonify(planets_response)
